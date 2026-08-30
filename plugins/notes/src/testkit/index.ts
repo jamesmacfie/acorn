69ec4fb312d44ff011a8fa0b23725d8c03398790
James Macfie <1654201+jamesmacfie@users.noreply.github.com>
2026-08-30 20:55:56 +1200

    chore(notes): retire main/

--- plugins/notes/src/testkit/index.ts
+++ plugins/notes/src/testkit/index.ts
@@ -3,5 +3,5 @@
 //   apps/node/test/integration/coreTools.test.ts     notesAgentTools, NotesStore
 //   apps/node/test/integration/taskContext.test.ts   NotesStore
 //   apps/node/test/integration/workflowRunner.test.ts   NotesStore
-export { notesAgentTools } from '../main/agentTools'
-export { NotesStore } from '../main/notes'
+export { notesAgentTools } from '../server/agentTools'
+export { NotesStore } from '../server/notes'
